8f493e7d3c10ce518cb3e0614e5e1b7b71660c76
itsnaannccy <70286081+itsnaannccy@users.noreply.github.com>
2021-02-09 22:23:35 -0800

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,27 @@
+// task 3: psuedocode a computer task and turning it into comments
+//
+
+//Hangman Game:
+
+  //Come up with a word at random
+  // Display the length of the word to the user
+  // Correct_guesses is less than the length of the word
+    // Tell the user to guess a letter
+      // If the user guesses a correct letter, then user earns a point for correct_guesses
+      // If the user guesses an incorrect letter, then the user earns a point for incorrect_guesss and draw the next part of hangman.
+  // If the users incorrect_guesses are greater than 8, then the user has lost the game
+  // If the users correct_guesses are equal to the length of the word, then the user has won the game
+
+//Performing CPR
+
+  // Check if there is a qualified professional around
+  // Lay person on their back and open airways
+  // Make sure if they are breathing
+  // If person is not breathing proceed to begin CPR
+  // Start with 30 chest compressions
+    // Use 2 hands for adults
+    // 1 hand for children
+    // 2 fingers for infants
+  // Let chest rise in between each compression
+  // Then do two rescue breaths
+  // Repeat process until a Medical Professional arrives on scene
