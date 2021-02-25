721a23ab21aa699c51cf8ca4ea48dc527f2dc715
itsnaannccy <70286081+itsnaannccy@users.noreply.github.com>
2021-02-25 15:48:18 -0800

    lab7 fixed

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -8,25 +8,26 @@
 /******************************************************************************/
 // Create a new function in your file, call it what you like, but choose wisely.
   function sortName() {
-// declare a variable userName and use window.prompt() to get the user's name from the user.
-  var userName = window.prompt("Please enter your user name:");
-  console.log("userName = ", userName);
-// Sort the letters of the user's name and return those from the function.
-  //convert userName to an array
-  var nameArray = userName.split('');
-  console.log("nameArray = ", nameArray);
-  //reverse to array
-  var sortNameArray = nameArray.sort();
-  console.log("sortNameArray = ", sortNameArray);
-  //convert the array back to a string
-  var nameSorted = sortNameArray.join('');
-  console.log("nameSorted = ", nameSorted);
-  //return the new string
-  return nameSorted;
+  // declare a variable userName and use window.prompt() to get the user's name from the user.
+    var userName = window.prompt("Please enter your user name:");
+    console.log("userName = ", userName);
+  // Sort the letters of the user's name and return those from the function.
+    //convert userName to an array
+    var nameArray = userName.split('');
+    console.log("nameArray = ", nameArray);
+    //reverse to array
+    var sortNameArray = nameArray.sort();
+    console.log("sortNameArray = ", sortNameArray);
+    //convert the array back to a string
+    var nameSorted = sortNameArray.join('');
+    console.log("nameSorted = ", nameSorted);
+    //return the new string
+    return nameSorted;
 }
 // Outside of the function, call the function and output the results with
 //document.writeln() to neatly output the user's sorted name.
-  document.writeln("Here is your new name: " + sortName() + "</br");
+  document.writeln("Here is your new sorted username: " + sortName() + "</br");
+  document.writeln("Here is your new sorted name: " + revName + "</br>");
 /******************************************************************************/
 /******************************************************************************/
 // Task X: Bonus #5
@@ -50,8 +51,10 @@
       return newStr;
   }
   // variable called revName to call sortUserName function
-  var revName = sortUserName(name);
-  document.writeln("Here is your new name: ", revName, "</br");
+    var revName = sortUserName(name);
+    // document.writeln("Here is your new name: "+ revName + "</br");
+    //the document.writeln above wouln't show up on the webpage, when i moved it
+    //above it showed up.
 
 /******************************************************************************/
 /******************************************************************************/
